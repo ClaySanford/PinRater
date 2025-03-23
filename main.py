42948f78ff8a23df587b87b577f40f3e95a98bac
Clay <claytheoneandonly@gmail.com>
2025-03-23 15:09:55 -0500

    Bot launches properly (not in Jupyter), can read messages

--- main.py
+++ main.py
@@ -11,11 +11,30 @@ except:
     print("Could not load TOKEN file. Shutting down...")
     sys.exit()
 
+client = discord.Client(intents=discord.Intents.default())
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content[0] != "!":
+        return
+    parsed_message = message.content.lower().split()
+    print(message.author.name + ": " + " ".join(parsed_message))
+    if (parsed_message == "!get"):
+        print("Get it!")
+
 
 
 #**************************************
 #RUN BOT:
+
 try:
-    clinet.run(TOKEN)
+    client.run(TOKEN)
 except:
     client.close()
+
