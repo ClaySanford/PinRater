6f71cb6f9dba4ff2715629efec90cfa3253887ca
Clay <claytheoneandonly@gmail.com>
2025-03-23 23:59:14 -0500

    Updated bot to display pairs and call a vote - not yet able to read vote

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import os
 import discord
 import random
-import ranking.py
+import rank as rank
+
+#global variables (kill me over it):
+IDList = []
+NAME_TOKEN = "general"
+EMOJI1 = ":red_square:"
+EMOJI2 = ":blue_square:"
+EMOJI1SLASH = "🟥"
+EMOJI2SLASH = "🟦"
+
 #Get API key from API.env (This is not the safest way to do this)
 try:
     f = open("API.env", "r")
-    TOKEN = f.read()
+    TOKEN = str(f.read())
     f.close()
 except:
     print("Could not load TOKEN file. Shutting down...")
@@ -13,8 +22,8 @@ except:
 
 try:
     f = open("guild.env", 'r')
-    GUILD = f.read()
-    f.close
+    GUILD = int(f.read())
+    f.close()
 except:
     print("Could not load GUILD file. Shutting down...")
     exit()
@@ -29,7 +38,16 @@ except:
     print("Could not load CHANNELS file. Shutting down...")
     exit()
 
-async def PinGrab(message):
+try:
+    f = open("user.env", 'r')
+    USER_TOKEN = str(f.read())
+    f.close()
+except:
+    print("Could not load USER file. Shutting down...")
+    exit()
+    
+
+async def PinGrab(message): #Grab all pins from every chat with "general" in the name
     ExtPins = []
     #Put all existing pins into array:
     reader = open("PinList.txt", 'r')
@@ -37,7 +55,7 @@ async def PinGrab(message):
         ExtPins.append(line)
     reader.close()
     for channel in message.guild.channels:
-        if "general" in channel.name:
+        if "NAME_TOKEN" in channel.name:
             AllPins = await channel.pins()
             writer = open("PinList.txt", 'a')
             print(AllPins[0])
@@ -48,7 +66,7 @@ async def PinGrab(message):
                     writer.write("\n")
             writer.close()
     
-def PinSani(message):
+def PinSani(message): #Format discord message to be style of name: message fileurl fileurl... fileurl
     attachments = message.attachments
     ImgCount = len(attachments)
     content = message.author.name + ": " + message.content
@@ -56,22 +74,27 @@ def PinSani(message):
         content = content + " " + str(attachments[i])
     return content
 
-async def GetMsg(GenChans, ID):
+async def GetMsg(ID): #Check all given general channels for the specified comment. Raise an error if message not in any channels.
+    ID = int(ID)
+    print(GenChans)
     for channel in GenChans:
         try:
+            print(channel)
             message = await channel.fetch_message(id=ID)
             return message
         except:
             pass
+    print(ID)
+    print(type(ID))
     raise MsgNotFound
 
-async def PrintList(channel, GenChans):
+async def PrintList(channel): #DEBUG: print all of a list
     reader = open("PinList.txt", 'r')
     found = 0
     notfound = 0
     for line in reader:
         try: 
-            message = await GetMsg(GenChans, line)
+            message = await GetMsg(line)
             found += 1
             print(str(found) + " messages found!")
             message = PinSani(message)
@@ -79,34 +102,123 @@ async def PrintList(channel, GenChans):
         except:
             notfound += 1
             print(str(notfound) + " messages not found.")
-            LostIDs.append(line)
 
+async def GetVote(Pair, channel, match): #Sets up the vote
+    match = str(match)
+    await channel.send(EMOJI1 + "#############################################################################################" + EMOJI2)
+    prologue = "Match " + match + "!"
+    await channel.send(content=prologue)
+
+    if (Pair.A == Pair.B): #If both items are the same, this isn't a vote, this is an empty round (rng do be like that)
+        await channel.send(content="This is an empty round! Oops!")
+        return
+    
+    if (Pair.A == 0):
+        try:
+            await channel.send(content="This is a bye round! Does this pin deserve to live?")
+            WinMsg = await GetMsg(Pair.B)
+            WinMsgTxt = EMOJI1 +  PinSani(WinMsg)
+            LoseMsgTxt = EMOJI2 + "Delete this pin!"
+            await channel.send(content=WinMsgTxt)
+            await channel.send(LoseMsgTxt)
+        except:
+            print("Failed to load B")
+        
+
+    elif (Pair.B == 0):
+        try:
+            await channel.send(content="This is a bye round! Does this pin deserve to live?")
+            WinMsg = await GetMsg(Pair.A)
+            WinMsgTxt = EMOJI2 + PinSani(WinMsg)
+            LoseMsgTxt = EMOJI1 + "Delete this pin!"
+            await channel.send(WinMsgTxt)
+            await channel.send(LoseMsgTxt)
+
+        except:
+            ("Failed to load A")
+
+    else:
+        try:
+            Msg1 = await(GetMsg(Pair.A))
+            Msg1Txt = EMOJI1 + PinSani(Msg1)
+            Msg2 = await(GetMsg(Pair.B))
+            Msg2Txt = EMOJI2 + PinSani(Msg2)
+            await channel.send("Which message should move forward?")
+            await channel.send(Msg1Txt)
+            await channel.send(Msg2Txt)
+        except:
+            print("failed to load one of two")
+    
+    VoteMsg = await channel.send("Vote here!")
+    await VoteMsg.add_reaction(EMOJI1SLASH)
+    await VoteMsg.add_reaction(EMOJI2SLASH)      
+
+
+async def PrintWinner(winner, channel):
+    print("Winner") #TODO: make the winner print
 
 client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
+    random.seed()
+    Gen1 = await client.fetch_channel(CHANS[0])
+    Gen2 = await client.fetch_channel(CHANS[1])
+    Gen3 = await client.fetch_channel(CHANS[2])
+    Gen4 = await client.fetch_channel(CHANS[3])
+    Gen5 = await client.fetch_channel(CHANS[4])
+    global GenChans
+    GenChans = [Gen1, Gen2, Gen3, Gen4, Gen5]
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == client.user: #Don't loop
         return
-    if message.content[0] != "!":
+    if message.content[0] != "!": #! os command character
         return
+
     parsed_message = message.content.lower().split()
-    print(message.author.name + ": " + " ".join(parsed_message))
-    if (parsed_message[0] == "!get"):
+    print(message.author.name + ": " + " ".join(parsed_message)) #Display the message and who sent it on terminal
+        
+    if (parsed_message[0] == "!get"): #Get all pinned messages from all text channels with the token, store in a PinList.txt
         await PinGrab(message)
-    if (parsed_message[0] == "!print"):
+        
+        
+    if (parsed_message[0] == "!print"): #Debug printing
         Gen1 = await client.fetch_channel(CHANS[0])
         Gen2 = await client.fetch_channel(CHANS[1])
         Gen3 = await client.fetch_channel(CHANS[2])
         Gen4 = await client.fetch_channel(CHANS[3])
         Gen5 = await client.fetch_channel(CHANS[4])
         GenChans = [Gen1, Gen2, Gen3, Gen4, Gen5]
-        await PrintList(message.channel, GenChans)
-        print(LostIDs)
+        await PrintList(message.channel)
+        
+
+    if ((parsed_message[0] == "!bracket") & (str(message.author.name) == USER_TOKEN)): #If admin says to, shuffle the bracket and fill it out to a power of 2
+        reader = open("PinList.txt", 'r')
+        #print("Heard Bracket")
+        for line in reader:
+            IDList.append(int(line))
+        #print(IDList)
+        #count = len(IDList)
+        #print(count)
+        rank.BracketFill(IDList)
+        #print(IDList)
+        rank.BracketShuffle(IDList)
+        #print(IDList)
+
+    if ((parsed_message[0] == "!rank") & (str(message.author.name) == USER_TOKEN)): #If admin says to, display the ranked options
+        #print("Heard")
+        PairList = []
+        if (len(IDList) > 1):
+            for i in range(int(len(IDList)/2)):
+                print(i)
+                PairList.append(rank.pair(IDList[i*2], IDList[i*2 + 1]))
+            for i in range(len(PairList)):
+                await GetVote(PairList[i], message.channel, i)
+        else:
+            await PrintWinner(IDList[0], message.channel)
 
 
 #**************************************
